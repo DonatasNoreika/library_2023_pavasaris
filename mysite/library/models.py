063637472a23ea68999a3da65b437afaaf39c0fd
Donatas Noreika <donoras@gmail.com>
2023-04-25 09:52:21 +0300

    3: +pakeisti modelių atvaizdavimo pavadinimai (vns. ir dgs.)

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -9,6 +9,10 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Žanras"
+        verbose_name_plural = 'Žanrai'
+
 
 class Author(models.Model):
     first_name = models.CharField(verbose_name="Vardas", max_length=50)
@@ -16,6 +20,11 @@ class Author(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Autorius"
+        verbose_name_plural = 'Autoriai'
+
 
 class Book(models.Model):
     title = models.CharField(verbose_name="Pavadinimas", max_length=100)
@@ -26,6 +35,11 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.author})"
+
+    class Meta:
+        verbose_name = "Knyga"
+        verbose_name_plural = 'Knygos'
+
 
 class BookInstance(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4)
@@ -43,3 +57,7 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return f"{self.book.title}: {self.uuid} ({self.due_back}) - {self.status}"
+
+    class Meta:
+        verbose_name = "Knygos egzempliorius"
+        verbose_name_plural = 'Knygos egzemplioriai'
