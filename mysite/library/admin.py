70cfa7e2b240a50bfe521f9dc8fdbc7ae6382026
Donatas Noreika <donoras@gmail.com>
2023-04-25 10:52:34 +0300

    3: +Prie knygų pridėta paieška pagal pavadinimą ir autorių

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from . import models
 
+
 class BookInstanceInline(admin.TabularInline):
     model = models.BookInstance
     extra = 0
     readonly_fields = ['uuid']
 
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'isbn', 'author', 'display_genre']
     inlines = [BookInstanceInline]
+    search_fields = ['title', 'author__first_name', 'author__last_name']
+
 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ['book', 'uuid', 'status', 'due_back']
@@ -19,8 +23,10 @@ class BookInstanceAdmin(admin.ModelAdmin):
         ('Availability', {'fields': ('status', 'due_back')}),
     )
 
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'display_books']
+
 
 # Register your models here.
 admin.site.register(models.Author, AuthorAdmin)
