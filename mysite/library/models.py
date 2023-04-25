026c70ed9b6e81446e7ebbdc81f4a6f1b05d8eb5
Donatas Noreika <donoras@gmail.com>
2023-04-25 10:07:15 +0300

    3: +sukurtas BookAdmin su žanrų atvaizdavimu per metodą

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -33,6 +33,11 @@ class Book(models.Model):
     author = models.ForeignKey(to='Author', verbose_name="Autorius", on_delete=models.SET_NULL, null=True)
     genre = models.ManyToManyField(to='Genre', verbose_name='Žanras')
 
+    def display_genre(self):
+        return ", ".join(genre.name for genre in self.genre.all())
+
+    display_genre.short_description = "Žanrai"
+
     def __str__(self):
         return f"{self.title} ({self.author})"
 
